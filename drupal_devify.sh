44445b3827e310a54dd6b0ab48adc878e76f04de
Jean-Francois Monfort <jeanfrancois@k-cy.com>
2013-11-26 21:40:12 +0100

    First commit

---
+++ drupal_devify.sh
@@ -0,0 +1,14 @@
+#! /bin/sh
+
+# Reset pwd admin 
+drush upwd admin --password=admin
+
+# Disabled cron job
+drush vset cron_safe_threshold 0
+
+ # Reset cache & preprocess
+drush vset preprocess_css 0
+drush vset preprocess_js 0
+drush vset page_compression 0
+drush vset cache 0
+
